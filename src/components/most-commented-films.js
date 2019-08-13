2757a21b54aede3214ecf1b4de2e28b8becf5ab6
bogdanGR <fant1k450@gmail.com>
2019-08-13 17:44:30 +0300

    СРÐДобави â НÐWebpack settings,separete to components and render

---
+++ src/components/most-commented-films.js
@@ -0,0 +1,7 @@
+export const createMostCommentedTemplate = () => `
+  <section class="films-list--extra  films-list--right">
+    <h2 class="films-list__title">Most commented</h2>
+  </section>
+ `;
+
+
---
+++ src/components/top-rated-films.js
@@ -0,0 +1,5 @@
+export const createTopRatedTemplate = () => `
+  <section class="films-list--extra  films-list--left">
+    <h2 class="films-list__title">Top rated</h2>
+  </section>
+ `;
