8c48f59cfb2de26235c62db131717244e6a7344b
bogdanGR <fant1k450@gmail.com>
2019-09-03 09:31:03 +0300

    fix sorting, refactore page-controller

--- src/components/page-controller.js
+++ src/components/page-controller.js
@@ -1,4 +1,4 @@
-import {getCards, getFilmCard} from "./data";
+import {getCards} from "./data";
 import {Card} from "./film-card";
 import {Popup} from "./popup";
 import {isDeactivateEvent, renderComponent} from "../utils";
@@ -10,6 +10,9 @@ import {SortTemplate} from "./sort-filter";
 import {Films} from "./films";
 import {FilmsList} from "./filmsList";
 
+const NUM_OF_RENDERING_TO_CATEGORIES = 2;
+const MAX_RENDER_CARDS = 5;
+
 export class PageController {
   constructor(container, cardData) {
     this._container = container;
@@ -18,48 +21,47 @@ export class PageController {
     this._filmsList = new FilmsList();
     this._numberOfCards = 30;
     this._sort = new SortTemplate();
+    this._btnShowMore = new Button();
+    this._topRatedFilms = new TopRatedFilms();
+    this._mostCommentedFilms = new MostCommented();
+    this._getCardsToCategories = getCards(NUM_OF_RENDERING_TO_CATEGORIES);
+    this._numCardsToRender = this._cardData.slice(0, MAX_RENDER_CARDS);
   }
   init() {
-    const NUM_OF_RENDERING_TO_CATEGORIES = 2;
-    const MAX_RENDER_CARDS = 5;
-    const getCardsToCategories = getCards(NUM_OF_RENDERING_TO_CATEGORIES);
-    const btnShowMore = new Button();
-    const topRatedFilms = new TopRatedFilms();
-    const mostCommentedFilms = new MostCommented();
+    const mainSite = document.querySelector(`.main`);
     if (this._numberOfCards) {
-      const numCardsToRender = this._cardData.slice(0, MAX_RENDER_CARDS);
+      renderComponent(mainSite, this._sort.getElement(), `beforeend`);
       renderComponent(this._container, this._films.getElement(), `beforeend`);
       const filmContainer = document.querySelector(`.films-list`);
       renderComponent(filmContainer, this._filmsList.getElement(), `beforeend`);
-      renderComponent(filmContainer, btnShowMore.getElement(), `beforeend`);
-      renderComponent(this._films.getElement(), topRatedFilms.getElement(), `beforeend`);
-      renderComponent(this._films.getElement(), mostCommentedFilms.getElement(), `beforeend`);
-      numCardsToRender.forEach((cardTpl) => this._renderCard(this._filmsList.getElement(), cardTpl));
-      const mainSite = document.querySelector(`.main`);
+      renderComponent(filmContainer, this._btnShowMore.getElement(), `beforeend`);
+      renderComponent(this._films.getElement(), this._topRatedFilms.getElement(), `beforeend`);
+      renderComponent(this._films.getElement(), this._mostCommentedFilms.getElement(), `beforeend`);
+      this._numCardsToRender.forEach((cardTpl) => this._renderCard(this._filmsList.getElement(), cardTpl));
       let cardsOnPage = MAX_RENDER_CARDS;
-      let leftCardToRender = numCardsToRender.length - cardsOnPage;
-      const loadMoreButton = mainSite.querySelector(`.films-list__show-more`);
+      let leftCardToRender = this._numCardsToRender.length - cardsOnPage;
+      const loadMoreButton = document.querySelector(`.films-list__show-more`);
 
       const onLoadMoreButtonClick = () => {
-        const cardsMocksMore = new Array(MAX_RENDER_CARDS).fill(``).map(getFilmCard);
-        cardsMocksMore.forEach((cardTpl) => this._renderCard(this._filmsList.getElement(), cardTpl));
+        this._numCardsToRender.forEach((cardTpl) => this._renderCard(this._filmsList.getElement(), cardTpl));
         cardsOnPage += MAX_RENDER_CARDS;
         leftCardToRender = this._numberOfCards - cardsOnPage;
-
         if (leftCardToRender <= 0) {
           loadMoreButton.classList.add(`visually-hidden`);
           loadMoreButton.removeEventListener(`click`, onLoadMoreButtonClick);
         }
       };
       loadMoreButton.addEventListener(`click`, onLoadMoreButtonClick);
-      getCardsToCategories.forEach((card) => this._renderCard(topRatedFilms.getElement().querySelector(`.films-list--left .films-list__container`), card));
-      getCardsToCategories.forEach((card) => this._renderCard(mostCommentedFilms.getElement().querySelector(`.films-list--right .films-list__container`), card));
-      this._sort.getElement().addEventListener(`click`, (evt) => this._onSortLinkClick(evt));
+      this._getCardsToCategories.forEach((card) => this._renderCard(this._topRatedFilms.getElement().querySelector(`.films-list--left .films-list__container`), card));
+      this._getCardsToCategories.forEach((card) => this._renderCard(this._mostCommentedFilms.getElement().querySelector(`.films-list--right .films-list__container`), card));
     } else {
       const noFilms = new NoFilms();
       renderComponent(this._container, noFilms.getElement(), `beforeend`);
     }
-
+    this._sort.getElement().addEventListener(`click`, (evt) => this._onSortLinkClick(evt));
+  }
+  _getCountCurrentCards() {
+    return this._container.querySelector(`.films-list__container`).querySelectorAll(`.film-card`).length;
   }
   _renderCard(localContainer, cardsTpl) {
     const card = new Card(cardsTpl);
@@ -99,19 +101,22 @@ export class PageController {
     if (evt.target.tagName !== `A`) {
       return;
     }
+    const countCurrentCards = this._getCountCurrentCards();
     this._filmsList.getElement().innerHTML = ``;
-
     switch (evt.target.dataset.sortType) {
       case `date`:
-        const sortedByDateCards = this._cardData.slice().sort((a, b) => a.year - b.year);
+        const sortFilmsByDate = this._cardData.slice().sort((a, b) => a.year - b.year);
+        const sortedByDateCards = sortFilmsByDate.slice(0, countCurrentCards);
         sortedByDateCards.forEach((card) => this._renderCard(this._filmsList.getElement(), card));
         break;
       case `rating`:
-        const sortedByRating = this._cardData.slice().sort((a, b) => a.rating - b.rating);
+        const sortFilmsByRating = this._cardData.slice().sort((a, b) => a.rating - b.rating);
+        const sortedByRating = sortFilmsByRating.slice(0, countCurrentCards);
         sortedByRating.forEach((card) => this._renderCard(this._filmsList.getElement(), card));
         break;
       case `default`:
-        this._cardData.forEach((cardTpl) => this._renderCard(this._filmsList.getElement(), cardTpl));
+        const sortByDefault = this._cardData.slice(0, countCurrentCards);
+        sortByDefault.forEach((cardTpl) => this._renderCard(this._filmsList.getElement(), cardTpl));
         break;
     }
   }
